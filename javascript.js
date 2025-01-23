bff4aa070413ee2ce7562313e5153e4a61296b56
Jbishopdev <osrsironmangim@gmail.com>
2025-01-23 13:35:44 -0500

    Edit styles, change number display logic

--- javascript.js
+++ javascript.js
@@ -1,3 +1,42 @@
+const displayBottom = document.querySelector("#display-bottom");
+const displayTop = document.querySelector("#display-top");
+const numberButton = document.querySelectorAll(".num");
+const operatorButton = document.querySelectorAll(".operator");
+let isAppending = true; // True if appending numbers, false if starting a new input
+let currentOperation = null; // Stores the selected operation
+let previousNumber = null; // To store the first operand
+
+function handleNumberClick(number) {
+  if (!isAppending) {
+    displayBottom.textContent = number; // Replace screen content for a new input
+    isAppending = true; // Start appending again after a reset
+  } else {
+    displayBottom.textContent += number; // Append to current input
+  }
+}
+
+function handleOperationClick(operation) {
+  previousNumber = displayBottom.textContent; // Store the number on the screen
+  currentOperation = operation; // Save the selected operation
+  isAppending = false; // Indicate that the next input will overwrite the screen
+}
+
+operatorButton.forEach ((button) => {
+  button.addEventListener('click', () => { 
+    handleOperationClick(button.textContent)
+  });
+  button.addEventListener("click", () => {
+    populateUpperDisplay(button.textContent);
+  })
+})
+
+
+numberButton.forEach ((button) => {
+  button.addEventListener('click', () => {
+    handleNumberClick(button.textContent)
+});
+});
+
 function add (a, b){
   let num1 = a;
   let num2 = b;
@@ -43,4 +82,14 @@ function operate(a, b, operator) {
     default:
       console.log("Invalid operator");
   }
+}
+
+function populateLowerDisplay(item) {
+  let num = item;
+  displayBottom.textContent = displayBottom.textContent + num;
+}
+
+function populateUpperDisplay(item) {
+  let operator = item;
+  displayTop.textContent = displayBottom.textContent + operator;
 }
